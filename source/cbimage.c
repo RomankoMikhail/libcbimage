61132a0289b877ab2f891275a117c46e3c67badd
Romanko Mikhail <romanko.mikhail@yandex.ru>
2017-04-22 19:16:12 +0300

    Functions responsible for bmp transferred to a separate file + security and overflows fixes

--- source/cbimage.c
+++ source/cbimage.c
@@ -21,214 +21,12 @@
  * SOFTWARE.
  */
 
-#include "../include/cbimage.h"
+#include <cbimage.h>
 
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
-
-#define LIBCBMP_BUFFER 1024
-
-
-typedef struct
-{
-	uint32_t	width;
-	uint32_t	height;
-	uint16_t	bpp;
-	off_t 		pointer_data;
-	int				valid;
-} lcbmp_header;
-
-
-
-
-off_t get_file_size(FILE *handle)
-{
-	off_t cur = ftell(handle), end;
-	fseek(handle, 0, SEEK_END);
-	end = ftell(handle);
-	fseek(handle, cur, SEEK_SET);
-	
-	return end;
-}
-
-
-
-
-lcbmp_header lcbmp_get_info(FILE *handle)
-{
-	lcbmp_header info = {0};
-	
-	off_t		file_size 		= get_file_size(handle);
-	off_t 	cur_position 	= ftell(handle);
-	
-	uint8_t bmp_header[54];
-	
-	if(file_size < 54)
-	{
-		return info;
-	}
-		
-	
-	fseek(handle, 0, SEEK_SET);
-	fread(bmp_header, 54, 1, handle);
-	
-	if(strncmp((char*)bmp_header, "BM", 2))
-	{
-		return info;
-	}
-	
-	if(*((uint32_t*)&bmp_header[2]) != file_size)
-	{
-		return info;
-	}
-	
-	info.pointer_data = *((uint32_t*)&bmp_header[10]);
-	info.width = *((uint32_t*)&bmp_header[18]);
-	info.height = *((uint32_t*)&bmp_header[22]);
-	info.bpp = *((uint16_t*)&bmp_header[28]);
-	
-	if(info.bpp != CBIMAGE_24BPP) {
-		return info;
-	}
-	
-	info.valid = 1;
-	fseek(handle, cur_position, SEEK_SET);
-	return info;
-}
-
-
-
-
-void lcbmp_form_info(uint8_t form[54], cbimage_t info)
-{	
-	off_t bmp_row = (((info.bpp * info.width + 31) >> 5) << 2);
-	size_t bmp_data = bmp_row * info.height;
-	
-	memset(form,0,54);
-	
-	strncpy((char*)form,"BM",2);
-	*((uint32_t*)&form[2]) = bmp_data + 54;
-	*((uint32_t*)&form[10]) = 54;
-	*((uint32_t*)&form[14]) = 40;
-	*((uint32_t*)&form[18]) = info.width;
-	*((uint32_t*)&form[22]) = info.height;
-	*((uint16_t*)&form[26]) = 1;
-	*((uint16_t*)&form[28]) = info.bpp;
-	
-	*((uint32_t*)&form[30]) = 0;
-	*((uint32_t*)&form[34]) = 0;
-	*((int32_t*)&form[38]) = 64;
-	*((int32_t*)&form[42]) = 64;
-	*((uint32_t*)&form[46]) = 0;
-	*((uint32_t*)&form[50]) = 0;
-}
-
-
-
-
-
-cbimage_t *cbimage_load_bmp(char *filename) 
-{
-	/* Optimize in futher implementations
-	 * 
-	 * uint8_t	bmp_buffer[LIBCBMP_BUFFER];
-	 * */
-	size_t 	current_row, current_col;
-	off_t		bmp_padding;
-	
-	cbimage_t			*loaded_image;
-	lcbmp_header 	header;
-	
-	FILE *handle;
-	
-	assert(filename != NULL);
-	
-	handle = fopen(filename, "rb");
-	if(!handle)
-	{
-		fprintf(stderr,"[ERROR] file \"%s\": ",filename);
-		perror("");
-		return NULL;
-	}
-	
-	header = lcbmp_get_info(handle);
-	if(!header.valid) {
-		fprintf(stderr,"[ERROR] file \"%s\": not valid\n",filename);
-		fclose(handle);
-		return NULL;
-	}
-	bmp_padding = (((header.bpp * header.width + 31) >> 5) << 2) - (header.width * (header.bpp >> 3));
-	
-	fseek(handle, header.pointer_data, SEEK_SET);
-	
-	loaded_image = calloc(1, sizeof(cbimage_t));
-	loaded_image->data = calloc(header.width * header.height, sizeof(uint8_t) * 3);
-	
-	for(current_row = 0; current_row < header.height; current_row++)
-	{
-		for(current_col = 0; current_col < header.width; current_col++)
-		{
-			uint8_t color[3];
-			fread(color, sizeof(uint8_t), 3, handle);
-			
-			loaded_image->data[(header.height - current_row - 1) * header.width * 3 + current_col*3 + 2] = color[0];
-			loaded_image->data[(header.height - current_row - 1) * header.width * 3 + current_col*3 + 1] = color[1];
-			loaded_image->data[(header.height - current_row - 1) * header.width * 3 + current_col*3] = color[2];
-			
-		}
-		fseek(handle, bmp_padding, SEEK_CUR);
-	}
-	loaded_image->bpp = header.bpp;
-	loaded_image->width = header.width;
-	loaded_image->height = header.height;
-	loaded_image->type = CBIMAGE_RGB;
-	
-	fclose(handle);
-	
-	return loaded_image;
-}
-
-
-
-
-
-int cbimage_save_bmp(char *filename, cbimage_t image)
-{
-	FILE 		*handle;
-	uint8_t header[54];
-	uint8_t zeros[3] = {0};
-	off_t 	bmp_padding = (((image.bpp * image.width + 31) >> 5) << 2) - (image.width * (image.bpp >> 3));
-	size_t	current_row, current_col;
-	
-	handle = fopen(filename, "wb");
-	if(!handle)
-	{
-		fprintf(stderr,"[ERROR] file \"%s\": ",filename);
-		perror("");
-		return -1;
-	}
-	
-	lcbmp_form_info(header, image);
-	
-	fwrite(header,sizeof(uint8_t), 54, handle);
-	for(current_row = 0; current_row < image.height; current_row++)
-	{
-		for(current_col = 0; current_col < image.width; current_col++)
-		{
-			fputc(image.data[(image.height - current_row - 1) * image.width * 3 + current_col * 3 + 2],handle);
-			fputc(image.data[(image.height - current_row - 1) * image.width * 3 + current_col * 3 + 1],handle);
-			fputc(image.data[(image.height - current_row - 1) * image.width * 3 + current_col * 3],handle);
-		}
-		fwrite(zeros,sizeof(uint8_t), bmp_padding, handle);
-	}
-	fclose(handle);
-	return 0;
-}
-
-
-
 
 
 int cbimage_inverse(cbimage_t *image)
@@ -238,7 +36,7 @@ int cbimage_inverse(cbimage_t *image)
 	
 	for(i = 0; i < image->height * image->width * ((image->bpp) >> 3); i++)
 	{
-		image->data[i] = 255 - image->data[i];
+		image->data[i] = 0xff - image->data[i];
 	}
 	return 0;
 }
@@ -416,11 +214,15 @@ int cbimage_free(cbimage_t *image)
 cbimage_t *cbimage_create(int width, int height, int bpp, int type)
 {
 	cbimage_t *new_image = calloc(1, sizeof(cbimage_t));
+	assert(new_image != NULL);
+
 	new_image->height = height;
 	new_image->width = width;
 	new_image->bpp = bpp;
 	new_image->type = type;
 	new_image->data = calloc(height * width * (bpp >> 3), sizeof(uint8_t));
+
+	assert(new_image->data != NULL);
 	return new_image;
 }
 
@@ -430,14 +232,17 @@ cbimage_t *cbimage_create(int width, int height, int bpp, int type)
 
 void cbimage_insert(cbimage_t *dst, cbimage_t *src, int x, int y) {
 	size_t ix, iy;
-	
+
 	for(ix = 0; ix < src->width; ix++) 
 	{
 		for(iy = 0; iy < src->height; iy++)
 		{
-			dst->data[CBIMAGE_INDEX(dst, (ix + x), (iy + y))] = src->data[CBIMAGE_INDEX(src, ix, iy)];
-			dst->data[CBIMAGE_INDEX(dst, (ix + x), (iy + y)) + 1] = src->data[CBIMAGE_INDEX(src, ix, iy) + 1];
-			dst->data[CBIMAGE_INDEX(dst, (ix + x), (iy + y)) + 2] = src->data[CBIMAGE_INDEX(src, ix, iy) + 2]; 
+			if(((ix + x) < dst->width) && ((iy + y) < dst->height) && ((iy + y) >= 0) && ((iy + y) >= 0))
+			{
+				dst->data[CBIMAGE_INDEX(dst, (ix + x), (iy + y))] = src->data[CBIMAGE_INDEX(src, ix, iy)];
+				dst->data[CBIMAGE_INDEX(dst, (ix + x), (iy + y)) + 1] = src->data[CBIMAGE_INDEX(src, ix, iy) + 1];
+				dst->data[CBIMAGE_INDEX(dst, (ix + x), (iy + y)) + 2] = src->data[CBIMAGE_INDEX(src, ix, iy) + 2]; 
+			}
 		}
 	}
 }
---
+++ source/cbimage_bmp.c
@@ -0,0 +1,217 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Romanko Mikhail
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+#include <cbimage.h>
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct
+{
+	uint32_t	width;
+	uint32_t	height;
+	uint16_t	bpp;
+	off_t 		pointer_data;
+	int				valid;
+} cbmp_header;
+
+
+
+off_t get_file_size(FILE *handle)
+{
+	off_t cur = ftell(handle), end;
+	fseek(handle, 0, SEEK_END);
+	end = ftell(handle);
+	fseek(handle, cur, SEEK_SET);
+	
+	return end;
+}
+
+
+
+
+cbmp_header cbmp_get_info(FILE *handle)
+{
+	cbmp_header info = {0};
+	
+	off_t		file_size 		= get_file_size(handle);
+	off_t 	cur_position 	= ftell(handle);
+	
+	uint8_t bmp_header[54];
+	
+	if(file_size < 54)
+	{
+		return info;
+	}
+		
+	
+	fseek(handle, 0, SEEK_SET);
+	fread(bmp_header, 54, 1, handle);
+	
+	if(strncmp((char*)bmp_header, "BM", 2))
+	{
+		return info;
+	}
+	
+	if(*((uint32_t*)&bmp_header[2]) != file_size)
+	{
+		return info;
+	}
+	
+	info.pointer_data = *((uint32_t*)&bmp_header[10]);
+	info.width = *((uint32_t*)&bmp_header[18]);
+	info.height = *((uint32_t*)&bmp_header[22]);
+	info.bpp = *((uint16_t*)&bmp_header[28]);
+	
+	if(info.bpp != CBIMAGE_24BPP) {
+		return info;
+	}
+	
+	info.valid = 1;
+	fseek(handle, cur_position, SEEK_SET);
+	return info;
+}
+
+
+
+
+void cbmp_form_info(uint8_t form[54], cbimage_t info)
+{	
+	off_t bmp_row = (((info.bpp * info.width + 31) >> 5) << 2);
+	size_t bmp_data = bmp_row * info.height;
+	
+	memset(form, 0, 54);
+	
+	strncpy((char*)form,"BM",2);
+	*((uint32_t*)&form[2]) = bmp_data + 54;
+	*((uint32_t*)&form[10]) = 54;
+	*((uint32_t*)&form[14]) = 40;
+	*((uint32_t*)&form[18]) = info.width;
+	*((uint32_t*)&form[22]) = info.height;
+	*((uint16_t*)&form[26]) = 1;
+	*((uint16_t*)&form[28]) = info.bpp;
+	*((int32_t*)&form[38])	= 64;
+	*((int32_t*)&form[42])	= 64;
+}
+
+
+
+
+
+cbimage_t *cbimage_load_bmp(char *filename) 
+{
+	/* Optimize in futher implementations
+	 * 
+	 * uint8_t	bmp_buffer[LIBCBMP_BUFFER];
+	 * */
+	size_t 	current_row, current_col;
+	off_t		bmp_padding;
+	
+	cbimage_t			*loaded_image;
+	cbmp_header 	header;
+	
+	FILE *handle;
+	
+	assert(filename != NULL);
+	
+	handle = fopen(filename, "rb");
+	if(!handle)
+	{
+		fprintf(stderr,"[ERROR] file \"%s\": ",filename);
+		perror("");
+		return NULL;
+	}
+	
+	header = cbmp_get_info(handle);
+	if(!header.valid) {
+		fprintf(stderr,"[ERROR] file \"%s\": not valid\n",filename);
+		fclose(handle);
+		return NULL;
+	}
+	bmp_padding = (((header.bpp * header.width + 31) >> 5) << 2) - (header.width * (header.bpp >> 3));
+	
+	fseek(handle, header.pointer_data, SEEK_SET);
+	loaded_image = cbimage_create(header.width, header.height, header.bpp, CBIMAGE_RGB);
+	
+	for(current_row = 0; current_row < header.height; current_row++)
+	{
+		for(current_col = 0; current_col < header.width; current_col++)
+		{
+			uint8_t color[3];
+			fread(color, sizeof(uint8_t), 3, handle);
+			
+			loaded_image->data[(header.height - current_row - 1) * header.width * 3 + current_col*3 + 2] = color[0];
+			loaded_image->data[(header.height - current_row - 1) * header.width * 3 + current_col*3 + 1] = color[1];
+			loaded_image->data[(header.height - current_row - 1) * header.width * 3 + current_col*3] = color[2];
+			
+		}
+		fseek(handle, bmp_padding, SEEK_CUR);
+	}
+	
+	fclose(handle);
+	
+	return loaded_image;
+}
+
+
+
+
+
+int cbimage_save_bmp(char *filename, cbimage_t image)
+{
+	FILE 		*handle;
+	uint8_t header[54];
+	uint8_t zeros[3] = {0};
+	off_t 	bmp_padding = (((image.bpp * image.width + 31) >> 5) << 2) - (image.width * (image.bpp >> 3));
+	size_t	current_row, current_col;
+	
+	handle = fopen(filename, "wb");
+	if(!handle)
+	{
+		fprintf(stderr,"[ERROR] file \"%s\": ",filename);
+		perror("");
+		return -1;
+	}
+	
+	cbmp_form_info(header, image);
+	
+	fwrite(header,sizeof(uint8_t), 54, handle);
+	for(current_row = 0; current_row < image.height; current_row++)
+	{
+		for(current_col = 0; current_col < image.width; current_col++)
+		{
+			fputc(image.data[(image.height - current_row - 1) * image.width * 3 + current_col * 3 + 2],handle);
+			fputc(image.data[(image.height - current_row - 1) * image.width * 3 + current_col * 3 + 1],handle);
+			fputc(image.data[(image.height - current_row - 1) * image.width * 3 + current_col * 3],handle);
+		}
+		fwrite(zeros,sizeof(uint8_t), bmp_padding, handle);
+	}
+	fclose(handle);
+	return 0;
+}
+
+
